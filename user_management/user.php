30867f3a62e2b26700608a24b7ecff9e0459afef
moomudmud <moomudkab@gmail.com>
2021-11-19 11:04:46 +0700

    update

--- user_management/user.php
+++ user_management/user.php
@@ -41,8 +41,9 @@ if (($_SESSION['role']<>'admin') OR empty($_SESSION['employee_id'])) {
                 <a class="navbar-brand" href="#">Mobile Access Control</a>
             </div>
             <ul class="nav navbar-nav">
-                <li ><a href="/MAC-Web/index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
-                <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">ผู้ดูแลระบบ <span class="caret"></span></a>
+                <li><a href="/MAC-Web/index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+                <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">ผู้ดูแลระบบ <span
+                            class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <li><a href="/MAC-Web/user_management/user.php">จัดการข้อมูลนักงาน</a></li>
                         <li><a href="#">Access Control</a></li>
@@ -52,49 +53,51 @@ if (($_SESSION['role']<>'admin') OR empty($_SESSION['employee_id'])) {
                 <li><a href="#">Page 2</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="/MAC-Web/register/user_information.php"><span class="glyphicon glyphicon-user"></span>  <?php echo $_SESSION["employee_id"];?></a></li>
-                <li><a href="/MAC-Web/register/logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+                <li><a href="/MAC-Web/register/user_information.php"><span class="glyphicon glyphicon-user"></span>
+                        <?php echo $_SESSION["employee_id"];?></a></li>
+                <li><a href="/MAC-Web/register/logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a>
+                </li>
             </ul>
         </div>
     </nav>
     <form action="" method="get">
-    <form action="" method="get">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12"> <br>
-                    <table class="table table-striped  table-hover table-responsive table-bordered">
-                        <thead>
-                            <h3>บัญชีรายชื่อพนักงาน</h3>
-                            <input type="search" name="q" class="form-control" placeholder="ใส่รหัสพนักงาน"> <br>
-                            <button type="submit" class="btn btn-primary">ค้นหาข้อมูล</button></a> <br>
-                </div>
-                <div class="col-md-12"> <br></div>
-                <tr>
-                    <th>รหัสพนักงาน</th>
-                    <th>ชื่อ</th>
-                    <th>นามสกุล</th>
-                    <th>เบอร์โทรศัพท์</th>
-                    <th>อีเมล</th>
-                    <th>
-                        <center>สถานะ</center>
-                    </th>
-                    <th>
-                        <center>Role</center>
-                    </th>
-                    <th>
-                        <center>สิทธิการเข้าถึง</center>
-                    </th>
-                    <th>
-                        <center>แก้ไข</center>
-                    </th>
-                    <th>
-                        <center>ลบ</center>
-                    </th>
+        <form action="" method="get">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12"> <br>
+                        <table class="table table-striped  table-hover table-responsive table-bordered">
+                            <thead>
+                                <h3>บัญชีรายชื่อพนักงาน</h3>
+                                <input type="search" name="q" class="form-control" placeholder="ใส่รหัสพนักงาน"> <br>
+                                <button type="submit" class="btn btn-primary">ค้นหาข้อมูล</button></a> <br>
+                    </div>
+                    <div class="col-md-12"> <br></div>
+                    <tr>
+                        <th>รหัสพนักงาน</th>
+                        <th>ชื่อ</th>
+                        <th>นามสกุล</th>
+                        <th>เบอร์โทรศัพท์</th>
+                        <th>อีเมล</th>
+                        <th>
+                            <center>สถานะ</center>
+                        </th>
+                        <th>
+                            <center>Role</center>
+                        </th>
+                        <th>
+                            <center>สิทธิการเข้าถึง</center>
+                        </th>
+                        <th>
+                            <center>แก้ไข</center>
+                        </th>
+                        <th>
+                            <center>ลบ</center>
+                        </th>
 
-                </tr>
-                </thead>
-                <tbody>
-                    <?php
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <?php
 
 
                     if (isset($_GET['q'])) {
@@ -132,38 +135,44 @@ if (($_SESSION['role']<>'admin') OR empty($_SESSION['employee_id'])) {
                                 <center><?= $k['role']; ?></center>
                             </td>
                             <td>
-                                <center><a href="/MAC-Web/user_management/access.php?employee_id=<?= $k['employee_id']; ?>&is_visitor=0" class="btn btn-info btn-sm">กำหนด</a></center>
+                                <center><a
+                                        href="/MAC-Web/user_management/access.php?employee_id=<?= $k['employee_id']; ?>&is_visitor=0"
+                                        class="btn btn-info btn-sm">กำหนด</a></center>
                             </td>
                             <td>
-                                <center><a href="/MAC-Web/user_management/edit_employee.php?employee_id=<?= $k['employee_id']; ?>" class="btn btn-warning btn-sm">แก้ไข</a></center>
+                                <center><a
+                                        href="/MAC-Web/user_management/edit_employee.php?employee_id=<?= $k['employee_id']; ?>"
+                                        class="btn btn-warning btn-sm">แก้ไข</a></center>
                             </td>
                             <td>
-                                <center><a href="delete_employess.php?employee_id=<?= $k['employee_id']; ?>" class="btn btn-danger btn-sm" onclick="return confirm('ยืนยันการลบข้อมูล !!');">ลบ</a></center>
+                                <center><a href="delete_employess.php?employee_id=<?= $k['employee_id']; ?>"
+                                        class="btn btn-danger btn-sm"
+                                        onclick="return confirm('ยืนยันการลบข้อมูล !!');">ลบ</a></center>
                             </td>
                         </tr>
 
                         </tr>
-                    <?php } ?>
-                </tbody>
-                </table>
-                <h3>สิทธิการเข้าถึง
-                    <table class="table table-striped  table-hover table-responsive table-bordered">
-                        <thead>
-                            <tr>
-                                <th>รหัสพนักงาน</th>
-                                <th>ชั้น</th>
-                                <th>ห้อง</th>
-                                <th>สถานะ</th>
-                                <th>
-                                    <center>อนุญาติ/ยกเลิก</center>
-                                </th>
+                        <?php } ?>
+                    </tbody>
+                    </table>
+                    <h3>สิทธิการเข้าถึง
+                        <table class="table table-striped  table-hover table-responsive table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>รหัสพนักงาน</th>
+                                    <th>ชั้น</th>
+                                    <th>ห้อง</th>
+                                    <th>สถานะ</th>
+                                    <th>
+                                        <center>อนุญาติ/ยกเลิก</center>
+                                    </th>
 
 
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <?php
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <?php
                                 if (isset($_GET['q'])) {
                                     $q = "%{$_GET['q']}%";
                                     $stmt = $conn->prepare("SELECT* 
@@ -192,28 +201,30 @@ if (($_SESSION['role']<>'admin') OR empty($_SESSION['employee_id'])) {
 
                                 foreach ($result as $k) {
                                 ?>
-                            <tr>
-                                <td><?= $k['employee_id']; ?></td>
-                                <td><?= $k['group_name']; ?></td>
-                                <td><?= $k['access_name']; ?></td>
-                                <td><?= $k['status_name']; ?></td>
-                                <td>
-                                    <center><a href="/MAC-Web/user_management/access_status.php?employee_id=<?= $k['employee_id']; ?>&access_name=<?= $k['access_name']; ?>&access_id=<?= $k['access_id']; ?>" class="btn btn-info btn-sm">ตั้งค่า</a></center>
-                                </td>
+                                <tr>
+                                    <td><?= $k['employee_id']; ?></td>
+                                    <td><?= $k['group_name']; ?></td>
+                                    <td><?= $k['access_name']; ?></td>
+                                    <td><?= $k['status_name']; ?></td>
+                                    <td>
+                                        <center><a
+                                                href="/MAC-Web/user_management/access_status.php?employee_id=<?= $k['employee_id']; ?>&access_name=<?= $k['access_name']; ?>&access_id=<?= $k['access_id']; ?>"
+                                                class="btn btn-info btn-sm">ตั้งค่า</a></center>
+                                    </td>
 
-                            </tr>
+                                </tr>
 
-                            </tr>
-                        <?php } ?>
+                                </tr>
+                                <?php } ?>
 
-                        </tr>
-                        </tbody>
-                    </table>
+                                </tr>
+                            </tbody>
+                        </table>
+                </div>
             </div>
-        </div>
-        </div>
+            </div>
 
-    </form>
+        </form>
 </body>
 
 </html>
